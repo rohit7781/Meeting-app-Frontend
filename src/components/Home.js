e06d9785625e17b1708a1a09d890b2af230a786a
Rohit Kumar <rohitkumar703314@gmail.com>
2022-01-26 22:57:20 +0530

    metitng

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ export const Home = (props) => {
 
         <div className='container my-5'>
 
-            {localStorage.getItem('token') ? <><AddNote showAlert={props.showAlert} /> </> : <><h2>Kindly Login or SignUp to Add your Notes or to view home Page</h2><Link className="btn btn-primary mx-1" to="/login" role="button">Login</Link>
+            {localStorage.getItem('token') ? <><AddNote showAlert={props.showAlert} /> </> : <><h2>Kindly Login or SignUp to Add your Meetings or to view home Page</h2><Link className="btn btn-primary mx-1" to="/login" role="button">Login</Link>
                 <Link className="btn btn-primary mx-1" to="/signup" role="button">Signup</Link></>}
         </div>
     )
